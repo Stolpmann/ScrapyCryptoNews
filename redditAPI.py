eaeb6f34eb0baca394b9ba8e4a486afca54aea7d
Stolpmann <evan.stolpmann@hotmail.com>
2021-08-02 12:23:11 -0400

    praw complete

--- redditAPI.py
+++ redditAPI.py
@@ -1,26 +1,32 @@
-import json
 import praw
-import requests
 
-subr = 'pythonsandlot'
-credentials = 'client_secrets.json'
+reddit = praw.Reddit(client_id="CDQv7Uy7ez0MGnGdRLzJlA",
+                     client_secret="xL99kLmt1TyBGRskB9_KtXU_EnR1iQ",
+                     user_agent="CryptoNewsAPI by u/thecoinisthefuture",
+                     password="123Blizzard!",
+                     username="thecoinisthefuture")
 
-with open(credentials) as f:
-    creds = json.load(f)
+subr = 'APItesttest'
 
-reddit = praw.Reddit(client_id=creds['client_id'],
-                     client_secret=creds['client_secret'],
-                     user_agent=creds['user_agent'],
-                     redirect_uri=creds['redirect_uri'],
-                     refresh_token=creds['refresh_token'])
+print(reddit.user.me())
+
 
 subreddit = reddit.subreddit(subr)
 
-title = 'Just Made My first Post on Reddit Using Python.'
-selftext = '''
-I am learning how to use the Reddit API with Python using the PRAW wrapper.
-By following the tutorial on https://www.jcchouinard.com/post-on-reddit-with-python-praw/
-This post was uploaded from my Python Script
-'''
+print(reddit.subreddit(subr))
 
-subreddit.submit(title, selftext=selftext)
+r = open("/Users/Evan/PycharmProjects/ScrapyCryptoNews/test2.txt", "r")
+
+posttitle, postinfo = r.readlines()
+
+print(posttitle)
+
+print(postinfo)
+
+#def join():
+#    string = ''.join(str(item) for item in postinfo)
+#    print(string)
+#join()
+
+
+subreddit.submit(posttitle, url=postinfo)
