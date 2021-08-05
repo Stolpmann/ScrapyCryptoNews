67d233965dd98da21f9a2dbdd9e003b06898b5b3
Stolpmann <evan.stolpmann@hotmail.com>
2021-08-05 17:06:24 -0400

    Added BTC Mag to API

--- redditPublish.py
+++ redditPublish.py
@@ -12,8 +12,10 @@ print(reddit.user.me())
 
 subreddit = reddit.subreddit(subr)
 
+
+#Cointelegraph Auto Uploader
+
 r = open("/Users/Evan/PycharmProjects/ScrapyCryptoNews/test.txt", "r")
-#print(r.read())
 
 lines = r.readlines()
 
@@ -24,10 +26,9 @@ second_last_lines = lines[-5:-3]
 print(last_lines)
 print(second_last_lines)
 
-
 def check():
     if last_lines != second_last_lines:
-        print("Lines are not equal")
+        print("\nLines are not equal\n")
 
         def append():
             string=''.join(str(item) for item in last_lines)
@@ -40,6 +41,39 @@ def check():
         append()
 
     elif last_lines == second_last_lines:
-        print("Lines are equal")
+        print("\nLines are equal\n")
 
 check()
+
+
+#Bitcoin Magazine Auto Uploader
+
+r2 = open("/Users/Evan/PycharmProjects/ScrapyCryptoNews/test2.txt", "r")
+
+lines2 = r2.readlines()
+
+last_lines2 = lines2[-2:]
+
+second_last_lines2 = lines2[-5:-3]
+
+print(last_lines2)
+print(second_last_lines2)
+
+def check2():
+    if last_lines2 != second_last_lines2:
+        print("\nLines are not equal\n")
+
+        def append2():
+            string2=''.join(str(item) for item in last_lines2)
+            a2 = string2.split("\n")
+            posttitle2, postinfo2 = a2[0],a2[1]
+            print(string2)
+            print(posttitle2)
+            print(postinfo2)
+            subreddit.submit(posttitle2, url=postinfo2)
+        append2()
+
+    elif last_lines2 == second_last_lines2:
+        print("\nLines are equal\n")
+
+check2()
--- rssScraper.py
+++ rssScraper.py
@@ -2,27 +2,62 @@ import feedparser
 
 url = "https://cointelegraph.com/rss"
 
+url2 = "https://bitcoinmagazine.com/.rss/full/"
+
 dir(feedparser)
 
 # Variable for Parsing Given URL
+
+# Cointelegraph
 f = feedparser.parse(url)
 
+# Bitcoin Magazine Data
+f2 = feedparser.parse(url2)
+
 # Opens test.txt for writing
-f2 = open("/Users/Evan/PycharmProjects/ScrapyCryptoNews/test.txt", "a")
+
+# Cointelegraph Data
+append = open("/Users/Evan/PycharmProjects/ScrapyCryptoNews/test.txt", "a")
+
+# Bitcoin Magazine Data
+append2 = open("/Users/Evan/PycharmProjects/ScrapyCryptoNews/test2.txt", "a")
+
 
 # Prints Parsed Data
+
+# Cointelegraph Data
 def scraperPrint():
     print(f.entries[0].title)
     print(f.entries[0].link)
 
+# Bitcoin Magazine Data
+def scraperPrint2():
+    print(f2.entries[0].title)
+    print(f2.entries[0].link)
+
 # Function writes Scraped Data to test.txt
+
+# Cointelegraph Feed
 def scraperWriteTitle():
-    f2.write("\n")
-    f2.write(f.entries[0].title)
-    f2.write("\n")
-    f2.write(f.entries[0].link)
-    f2.write("\n")
+    append.write("\n")
+    append.write(f.entries[0].title)
+    append.write("\n")
+    append.write(f.entries[0].link)
+    append.write("\n")
+
+# Bitcoin Magazine Feed
+def scraperWriteTitle2():
+    append2.write("\n")
+    append2.write(f2.entries[0].title)
+    append2.write("\n")
+    append2.write(f2.entries[0].link)
+    append2.write("\n")
+
 
 # Calling all functions
+
 scraperPrint()
 scraperWriteTitle()
+
+scraperPrint2()
+scraperWriteTitle2()
