72ad4341b69d8ebb3409f525cce0a9ce19930b53
Stolpmann <evan.stolpmann@hotmail.com>
2021-08-07 14:56:37 -0400

    added all 5 feeds

--- redditPublish.py
+++ redditPublish.py
@@ -112,64 +112,64 @@ check3()
 
 #Bitcoin Magazine Auto Uploader
 
-#r4 = open("/Users/Evan/PycharmProjects/ScrapyCryptoNews/test4.txt", "r")
+r4 = open("/Users/Evan/PycharmProjects/ScrapyCryptoNews/test4.txt", "r")
 
-#lines4 = r4.readlines()
+lines4 = r4.readlines()
 
-#last_lines4 = lines4[-2:]
+last_lines4 = lines4[-2:]
 
-#second_last_lines4 = lines4[-5:-3]
+second_last_lines4 = lines4[-5:-3]
 
-#print(last_lines4)
-#print(second_last_lines4)
+print(last_lines4)
+print(second_last_lines4)
 
-#def check4():
-    #if last_lines4 != second_last_lines4:
-        #print("\nLines are not equal\n")
+def check4():
+    if last_lines4 != second_last_lines4:
+        print("\nLines are not equal\n")
 
-        #def append4():
-            #string4=''.join(str(item) for item in last_lines4)
-            #a4 = string4.split("\n")
-            #posttitle4, postinfo4 = a4[0],a4[1]
-            #print(string4)
-            #print(posttitle4)
-            #print(postinfo4)
-            #subreddit.submit(posttitle4, url=postinfo4)
-        #append4()
+        def append4():
+            string4=''.join(str(item) for item in last_lines4)
+            a4 = string4.split("\n")
+            posttitle4, postinfo4 = a4[0],a4[1]
+            print(string4)
+            print(posttitle4)
+            print(postinfo4)
+            subreddit.submit(posttitle4, url=postinfo4)
+        append4()
 
-    #elif last_lines4 == second_last_lines4:
-        #print("\nLines are equal\n")
+    elif last_lines4 == second_last_lines4:
+        print("\nLines are equal\n")
 
-#check4()
+check4()
 
 #Bitcoin Magazine Auto Uploader
 
-#r5 = open("/Users/Evan/PycharmProjects/ScrapyCryptoNews/test5.txt", "r")
+r5 = open("/Users/Evan/PycharmProjects/ScrapyCryptoNews/test5.txt", "r")
 
-#lines5 = r5.readlines()
+lines5 = r5.readlines()
 
-#last_lines5 = lines5[-2:]
+last_lines5 = lines5[-2:]
 
-#second_last_lines5 = lines5[-5:-3]
+second_last_lines5 = lines5[-5:-3]
 
-#print(last_lines5)
-#print(second_last_lines5)
+print(last_lines5)
+print(second_last_lines5)
 
-#def check5():
-    #if last_lines5 != second_last_lines5:
-        #print("\nLines are not equal\n")
+def check5():
+    if last_lines5 != second_last_lines5:
+        print("\nLines are not equal\n")
 
-        #def append5():
-            #string5=''.join(str(item) for item in last_lines5)
-            #a5 = string5.split("\n")
-            #posttitle5, postinfo5 = a5[0], a5[1]
-            #print(string5)
-            #print(posttitle5)
-            #print(postinfo5)
-            #subreddit.submit(posttitle5, url=postinfo5)
-        #append5()
+        def append5():
+            string5=''.join(str(item) for item in last_lines5)
+            a5 = string5.split("\n")
+            posttitle5, postinfo5 = a5[0], a5[1]
+            print(string5)
+            print(posttitle5)
+            print(postinfo5)
+            subreddit.submit(posttitle5, url=postinfo5)
+        append5()
 
-    #elif last_lines5 == second_last_lines5:
-        #print("\nLines are equal\n")
+    elif last_lines5 == second_last_lines5:
+        print("\nLines are equal\n")
 
-#check5()
+check5()
