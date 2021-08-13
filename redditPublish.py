13c6c4d3295e5e18819343dd31a7715819d57e5f
Stolpmann <evan.stolpmann@hotmail.com>
2021-08-13 11:44:40 -0400

    added return to funcs

--- redditPublish.py
+++ redditPublish.py
@@ -6,13 +6,6 @@ reddit = praw.Reddit(client_id="-Ni4bYP9ySJYzRG2d8LOww",
                      password="123Blizzard",
                      username="thecanopenerpodcast")
 
-subr = 'CryptoCurrency'
-
-print(reddit.user.me())
-
-subreddit = reddit.subreddit(subr)
-
-
 #Cointelegraph Auto Uploader
 
 r = open("/Users/Evan/PycharmProjects/ScrapyCryptoNews/test.txt", "r")
@@ -31,20 +24,21 @@ def check():
         print("\nLines are not equal\n")
 
         def append():
+            subr = 'CryptoCurrency'
+            subreddit = reddit.subreddit(subr)
             string=''.join(str(item) for item in last_lines)
             a = string.split("\n")
             posttitle, postinfo = a[0],a[1]
             print(string)
             print(posttitle)
             print(postinfo)
-            subreddit.submit(posttitle, url=postinfo)
+            return subreddit.submit(posttitle, url=postinfo)
         append()
 
     elif last_lines == second_last_lines:
         print("\nLines are equal\n")
 
 check()
-
 
 #Bitcoin Magazine Auto Uploader
 
@@ -64,13 +58,15 @@ def check2():
         print("\nLines are not equal\n")
 
         def append2():
+            subr = 'CryptoCurrency'
+            subreddit = reddit.subreddit(subr)
             string2=''.join(str(item) for item in last_lines2)
             a2 = string2.split("\n")
             posttitle2, postinfo2 = a2[0],a2[1]
             print(string2)
             print(posttitle2)
             print(postinfo2)
-            subreddit.submit(posttitle2, url=postinfo2)
+            return subreddit.submit(posttitle2, url=postinfo2)
         append2()
 
     elif last_lines2 == second_last_lines2:
@@ -96,13 +92,15 @@ def check3():
         print("\nLines are not equal\n")
 
         def append3():
+            subr = 'CryptoCurrency'
+            subreddit = reddit.subreddit(subr)
             string3=''.join(str(item) for item in last_lines3)
             a3 = string3.split("\n")
             posttitle3, postinfo3 = a3[0],a3[1]
             print(string3)
             print(posttitle3)
             print(postinfo3)
-            subreddit.submit(posttitle3, url=postinfo3)
+            return subreddit.submit(posttitle3, url=postinfo3)
         append3()
 
     elif last_lines3 == second_last_lines3:
